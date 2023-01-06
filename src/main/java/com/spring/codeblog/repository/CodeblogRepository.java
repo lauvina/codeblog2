dac8cb907d6d9a5a135a9d321295537a68ab6eb0
claudiane.nascimento <claudi.1967@outlook.com>
2023-01-06 10:57:10 -0300

    Revert "Projeto spring mvc"
    This reverts commit ceb6307e5e7256fd982f769c7367b6e46411d374.

--- src/main/java/com/spring/codeblog/repository/CodeblogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spring.codeblog.repository;
-
-import com.spring.codeblog.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CodeblogRepository extends JpaRepository<Post, Long> {
-}
